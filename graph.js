e3ffd6b1f5ee510117d2e83218355b313fc35e97
Jeremy Sobczak <code@jerspace.com>
2023-08-09 14:38:20 -0400

    Added methods for Graph class.

--- graph.js
+++ graph.js
@@ -11,25 +11,78 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    for (let vertex of vertexArray) {
+      this.nodes.add(vertex);
+    }
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+      v1.adjacent.add(v2);
+      v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    for (neighbor in vertex.adjacent) {
+      neighbor.adjacent.delete(vertex);
+    }
+    this.nodes.delete(vertex);
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    let nodeValueArray = []
+    let toVisitStack = [start];
+    let seen = new Set(toVisitStack);
+
+    while (toVisitStack.length > 0) {
+      let currNode = toVisitStack.pop();
+
+      nodeValueArray.push(currNode.value);
+
+      for (let neighbor of currNode.adjacent) {
+        if (!seen.has(neighbor)) {
+          toVisitStack.push(neighbor);
+          seen.add(neighbor);
+        }
+      }
+    }
+    return nodeValueArray;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    let nodeValueArray = []
+    let toVisitQueue = [start];
+    let seen = new Set(toVisitQueue);
+
+    while (toVisitQueue.length > 0) {
+      let currNode = toVisitQueue.shift();
+
+      nodeValueArray.push(currNode.value);
+
+      for (let neighbor of currNode.adjacent) {
+        if (!seen.has(neighbor)) {
+          toVisitQueue.push(neighbor);
+          seen.add(neighbor);
+        }
+      }
+    }
+    return nodeValueArray;
+  }
 }
 
 module.exports = {Graph, Node}
